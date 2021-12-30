01ac3ca1963dac26626e785cb7f7191662339a71
Lynne Munini <muninilynne65@gmail.com>
2021-12-30 16:17:26 -0500

    Added posts

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+import requests
 app = Flask(__name__)
 
 
+posts = requests.get("https://api.npoint.io/9260e848de41c3dd6999").json()
+post_objects = []
+for post in posts:
+    post_objects.append(post)
+
+    
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=post_objects)
 
 @app.route('/about')
 def about():
