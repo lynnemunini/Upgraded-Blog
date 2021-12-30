29bb21d2bed00d813e5ccdc50abae8ffa433f805
Lynne Munini <muninilynne65@gmail.com>
2021-12-30 16:46:47 -0500

    Final Commit

--- main.py
+++ main.py
@@ -21,7 +21,13 @@ def about():
 def contact():
     return render_template("contact.html")
 
-
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
